61c72d0c1524d7041ad04b39343d25b1cacf355a
Nazar Hussain <nazarhussain@gmail.com>
2024-08-13 17:23:25 +0500

    Use historical regen object

--- packages/beacon-node/src/chain/interface.ts
+++ packages/beacon-node/src/chain/interface.ts
@@ -57,6 +57,7 @@ import {ShufflingCache} from "./shufflingCache.js";
 import {BlockRewards} from "./rewards/blockRewards.js";
 import {AttestationsRewards} from "./rewards/attestationsRewards.js";
 import {SyncCommitteeRewards} from "./rewards/syncCommitteeRewards.js";
+import {IHistoricalStateRegen} from "./historicalState/types.js";
 
 export {BlockType, type AssembledBlockType};
 export {type ProposerPreparationData};
@@ -95,6 +96,7 @@ export interface IBeaconChain {
   readonly clock: IClock;
   readonly emitter: ChainEventEmitter;
   readonly regen: IStateRegenerator;
+  readonly historicalStateRegen?: IHistoricalStateRegen;
   readonly lightClientServer?: LightClientServer;
   readonly reprocessController: ReprocessController;
   readonly pubkey2index: PubkeyIndexMap;
